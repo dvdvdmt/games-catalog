1dff0cda1edc9c199766e62470c419bbc32d4bf5
Dmitry Davydov <dvdvdmt@gmail.com>
2017-12-03 14:19:30 +0500

    Move template and styles to seed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 import { ApiService } from './shared';
 
-import '../style/app.scss';
+import '../style/app.css';
 
 @Component({
   selector: 'my-app', // <my-app></my-app>
@@ -10,7 +10,6 @@ import '../style/app.scss';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  url = 'https://github.com/preboot/angular2-webpack';
   title: string;
 
   constructor(private api: ApiService) {
